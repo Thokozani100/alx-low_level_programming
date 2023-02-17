6b57602a3ebdb4f5a3613def03c4aac642b452cb
Thokozani100 <thokozani100000@icloud.com>
2023-02-17 02:02:53 -0800

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)",sizeof(char));
+	printf("size of int: %lu byte(s),sizeof(int));
+	printf("size of a long int: %lu byte(s)",sizeof(long int));
+	printf("size of a float: %byte(s)",sizeof(float));
+	return(0);
+}
