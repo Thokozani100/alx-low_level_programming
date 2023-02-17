9b94739bdee48713466c8ffc29117a137673d0fb
Thokozani100 <thokozani100000@icloud.com>
2023-02-17 03:43:44 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main(){
+/**
+ * main - Entry pint
+ *
+ * Description: 'the program's desciption'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
 printf( " with proper grammar, but the outcome is a piece of art,");
 return 0;
 }
