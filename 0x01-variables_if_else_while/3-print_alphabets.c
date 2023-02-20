24fa5b2dfe88af933019c4a73c4505cd7ef90d85
Thokozani100 <thokozani100000@icloud.com>
2023-02-20 12:44:27 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 
 /**
+  * main - Prints the alphabetic in lower and upper case
+  *
+  * Return: Always (Success)
+  */
  * main - Prints the alphabetic in lower and upper case
  *
  * Return: Always (Success)
  */
 int main(void)
 {
+	char c, d;
 	char c;
 
+	d = 'z';
+
+	for (c = 'a'; c <= d; c++)
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 
+		if (c == 'z')
+		{
+			c = 'A';
+			d = 'Z';
+		}
 	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
 
 	putchar('\n');
-
-	return (0);
 }
-
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-
