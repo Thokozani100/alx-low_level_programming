dd8e1fd6cef1b8cee5f31f26c26342b050194348
Thokozani100 <thokozani100000@icloud.com>
2023-03-08 13:15:55 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * factorial - return factorial
+ * @n: return the factorial from
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+
