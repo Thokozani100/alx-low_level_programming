9864b3b6cf9a5bfe53d4755e7cdb7afd14008bb8
Thokozani100 <thokozani100000@icloud.com>
2023-03-16 16:38:58 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,19 +1,40 @@
 #include <stdlib.h>
+
 #include "main.h"
 
-/**
- *  malloc_check - allocates memory using malloc
- * @b: number of bytes to allocate
- * Return: a pointer to the allocated memory
- */
-void *malloc_check(unsigned int b)
-{
-	void  *ptr;
 
-	prt = malloc(b);
+
+/**
+
+ * *malloc_checked - main  allocates memory using malloc
+
+ * @b: number of bytes to allocate
+
+ *
+
+ * Return: a pointer to the allocated memory
+
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+	void *ptr;
+
+
+
+	ptr = malloc(b);
+
+
 
 	if (ptr == NULL)
+
 		exit(98);
 
+
+
 	return (ptr);
+
 }
+
