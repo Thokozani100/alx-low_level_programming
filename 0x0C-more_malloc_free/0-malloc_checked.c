9bbe81f65101f6336ba4c6264b3a717e7e06f067
Thokozani100 <thokozani100000@icloud.com>
2023-03-17 12:41:19 -0700

    *

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,18 +2,20 @@
 #include "main.h"
 
 /**
- *  malloc_check - allocates memory using malloc
+ * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
+ *
  * Return: a pointer to the allocated memory
  */
-void *malloc_check(unsigned int b)
+void *malloc_checked(unsigned int b)
 {
-	void  *ptr;
+	void *ptr;
 
-	prt = malloc(b);
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
 
 	return (ptr);
 }
+
