faa0b0beec0c2d92972cce9068a463107d865c2b
Thokozani100 <thokozani100000@icloud.com>
2023-04-04 14:09:48 -0700

    update

--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -5,6 +5,7 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -16,15 +17,21 @@ int main(void)
 
 	head = &hello;
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
+
 	{
+
 		printf("Error\n");
+
 			return (1);
+
 	}
+
 	new->n = 9;
 	new->next = head;
 	head = new;
-	n = print_listint(head);
+	n = listint_len(head);
 	printf("-> %lu elements\n", n);
 	free(new);
 	return (0);
